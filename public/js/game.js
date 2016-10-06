7fcba5d1e3f24d52dce438eeddb99eff47ecebba
Alex Craven <w.alex.craven@gmail.com>
2016-10-06 11:41:27 -0500

    implmented basic phaser render, corrected directiory name typo

--- public/js/game.js
+++ public/js/game.js
@@ -1,5 +1,9 @@
 "use strict";
 
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gameDiv');
-game.state.add('preload', preload);
+game.state.add('preloadState', preloadState);
+console.log('preloadState added');
 game.state.add('main', main);
+console.log('main added');
+
+game.state.start('preloadState');
--- public/js/main.js
+++ public/js/main.js
@@ -5,9 +5,22 @@ let main = {
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
     let player1 = game.add.sprite(100, 100, 'paddle');
+    console.log('player1', player1);
     let player2 = game.add.sprite(200, 200, 'paddle');
     let ball = game.add.sprite(300, 300, 'ball');
 
     let cursors = game.input.keyboard.createCursorKeys();
+  },
+  update: function() {
+    //movement
+    // player.body.velocity.y = 0;
+    // player.body.velocity.x = 0;
+    //
+    // if(cursors.up.isDown) {
+    //   player.body.velocity.y -= 100;
+    // }
+    // else if(cursors.down.isDown) {
+    //   player.body.velocity.y += 100;
+    // }
   }
 }
