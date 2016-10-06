3dd6f890b0335886a12ed976d51477ff25940c64
Alex Craven <w.alex.craven@gmail.com>
2016-10-06 14:33:30 -0500

    working local game w/ collisions

--- public/js/preload.js
+++ public/js/preload.js
@@ -4,10 +4,9 @@ let preloadState = {
   preload: function() {
     game.load.image('paddle', '../assets/paddle.png');
     game.load.image('ball', '../assets/ball.png');
-    console.log('preloaded');
+    game.load.image('edge', '../assets/edge.png');
   },
   create: function() {
 		game.state.start('main');
-    console.log('loading main');
 	}
 }
