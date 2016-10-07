4e4f3d977c822fac6cc48718b7f40310468a98ff
Alex Craven <w.alex.craven@gmail.com>
2016-10-07 15:47:59 -0500

    added menu state

--- public/js/preload.js
+++ public/js/preload.js
@@ -7,6 +7,16 @@ let preloadState = {
     game.load.image('edge', '../assets/edge.png');
   },
   create: function() {
-		game.state.start('main');
+		game.state.start('menu');
 	}
+}
+let menu = {
+  create: function() {
+    let text = game.add.text(225, 250, "Start Game", { font: "65px Arial", fill:'#fff', align: "center" });
+    text.inputEnabled = true;
+    text.events.onInputUp.add(menu.up);
+  },
+  up: function(item) {
+    game.state.start('main');
+  }
 }
